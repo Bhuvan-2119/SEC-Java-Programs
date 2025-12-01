53ccf850b3aefecb67a2c9db2572b4933dfd6049
Bhuvan Reddy <bhuvanreddy8933@gmail.com>
2025-12-01 21:22:35 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,23 @@
+class Rectangle{
+    int length , width ;
+
+Rectangle(int l , int w){
+    length = l;
+    width = w ;
+}
+
+Rectangle(int side){
+    length=width = side;
+    }
+int area(){
+    return length*width;
+    }
+}
+public class area{
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(15,8);
+        Rectangle r2 = new Rectangle(9);
+        System.out.println(r1.area());
+        System.out.println(r2.area());
+    }
+}
